e7de5fe696ffc419ad41fa55d52132029a2c29e4
Randall Coding <randallcoding@protonmail.com>
2024-04-03 12:14:54 +0700

    Handles edge cases on web app and updates wait message

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     # @bands = Band.all
-    folder_script_path = Rails.root.join('..', "coachella.gpt").to_s
-    # params[:input] = "DJ Snake"
-
+    # params[:input] = "DJ Snake"    
     # @bands = [{"name"=>"DJ Snake", "spotifyUrl"=>"https://open.spotify.com/artist/540vIaP2JwjQb9dm3aArA4", "songs"=>[{"name"=>"Lean On (feat. MØ & DJ Snake)", "url"=>"https://open.spotify.com/track/1qE47wUKG2juJwPoLqg4C9"}, {"name"=>"Let Me Love You", "url"=>"https://open.spotify.com/track/0lYBSQXN6rCTvUZvg9S0lU"}, {"name"=>"Please Don't Change (feat. DJ Snake)", "url"=>"https://open.spotify.com/track/0k0GtcnyQLMiXrdEDbLXmJ"}]}]
     # return :index
+    
+    folder_script_path = Rails.root.join('..', "coachella.gpt").to_s
 
     if params[:input].present?
       Dir.chdir(File.dirname(folder_script_path)) do
@@ -25,7 +25,8 @@ class HomeController < ApplicationController
           parsed_string = stdout.gsub("```json\n", "").gsub("\n```", "").strip
           json_output = JSON.parse(parsed_string)
           @bands = json_output
-          Rails.logger.info "GPTscript successfull.  Output is:"
+          @bands = [@bands] unless @bands.is_a? Array
+          Rails.logger.info "GPTscript successful.  Output is:"
           Rails.logger.info @bands
         else
           puts "Error: #{stderr}"
