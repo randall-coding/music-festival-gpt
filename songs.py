a8f07c6133c79a6d41a5f0078911d5fba281c9d3
Randall Coding <randallcoding@protonmail.com>
2024-04-03 11:36:30 +0700

    Updates Selenium arguments

--- songs.py
+++ songs.py
@@ -10,6 +10,7 @@ url = sys.argv[1]
 # Setup WebDriver options
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
+options.add_argument("--no-sandbox")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
 driver = webdriver.Chrome(options=options)
 
